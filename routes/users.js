d2b05e0c5a0488c9f20cdc64ccea15c7484fd754
clyde <40743130@gm.nfu.edu.tw>
2021-07-31 21:13:01 +0800

    create patch Method update data by ID

--- routes/users.js
+++ routes/users.js
@@ -42,4 +42,18 @@ router.delete('/:id', (req, res)=>{
     res.send(`User with the ID ${id} has been deleted`);
 })
 
+router.patch('/:id', (req, res)=>{
+    const { id } = req.params;
+    const {firstName, lastName, age} = req.body;
+
+    const user = users.find( (user)=> user.id === id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+    
+    res.send(`User with the ID ${id} has been updated`);
+
+});
+
 export default router;
