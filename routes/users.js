7d24d4d109b6ec2d984fbfd3cf43023d2effbf68
clyde <40743130@gm.nfu.edu.tw>
2021-07-31 20:57:01 +0800

    create get and delete by ID Method

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,12 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
 
 const router = express.Router();
 
 // mock database for
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "John",
-        lastName: "Ceeeena",
-        age: 77
-    }
-
-];
+let users = [];
 
 // all routes in here are starting with /users
 // because I set the routes beginning with /users in index.js
@@ -30,11 +21,25 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     // console.log("POST method has be Call");
     const user = req.body;
-    users.push(user);
+    users.push({ ...user, id: uuidv4()});
 
     // res.send("POST ROUTE REACHED");
     res.send(`User with the username ${user.firstName}`);
 });
 
+// /user/123 => req.params {id: 123}
+router.get('/:id', (req,res)=>{
+    const { id } = req.params;
+
+    const foundUser = users.find( user => user.id === id);
+
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res)=>{
+    const { id } = req.params;
+    users = users.filter( user => user.id !== id);
+    res.send(`User with the ID ${id} has been deleted`);
+})
 
 export default router;
